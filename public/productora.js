5c0a0bc52d906eb7b9ec5d6c57f0e0b3278cc7a2
Marco <matehuala0484@gmail.com>
2026-04-25 00:39:07 -0600

    fin

--- public/productora.js
+++ public/productora.js
@@ -96,9 +96,8 @@ function renderFeatures(features) {
   if (tag) {
     tag.innerHTML = "";
 
-    const headers = features.filter(
-      f => Number(f.level) === 1
-    );
+const headers = features.filter(f => Number(f.level) === 1);
+
 
     headers.forEach(header => {
       const div = document.createElement("div");
@@ -118,9 +117,7 @@ function renderFeatures(features) {
   if (badgesContainer) {
     badgesContainer.innerHTML = "";
 
-    const badges = features.filter(
-      f => Number(f.level) === 2
-    );
+const badges = features.filter(f => Number(f.level) === 2);
 
     badges.forEach(f => {
       const span = document.createElement("span");
@@ -134,9 +131,7 @@ function renderFeatures(features) {
   if (trustContainer) {
     trustContainer.innerHTML = "";
 
-    const trust = features.filter(
-      f => Number(f.level) === 3
-    );
+const trust = features.filter(f => Number(f.level) === 3);
 
     trust.forEach(f => {
       const div = document.createElement("div");
